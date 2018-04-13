5e10a38ab28849b5d453b134e1f042a3e9d7068f
Elcoosp <elcoosp@gmail.com>
2018-04-13 23:37:31 +0200

    spec: themmer add nested keys support

--- src/themmer/spec.js
+++ src/themmer/spec.js
@@ -1,16 +1,28 @@
 const themmer = require('./index')
-test('should return an object with a function for each key in the theme object, this function when provided a prop will look at the prop.theme[functitonNama][functionArgument]', () => {
-  const theme = {
-    color: {
-      main: 'red',
-      secondary: 'black'
-    },
-    padding: { small: '3px' }
-  }
 
-  const { color, padding } = themmer(theme)
-  const props = { theme }
+const theme = {
+  color: {
+    main: 'red',
+    secondary: 'black'
+  },
+  blob: {
+    foo: {
+      bar: 'yolo'
+    }
+  },
+  padding: { small: '3px' }
+}
+
+const props = { theme }
+
+const { color, padding, blob } = themmer(theme)
+
+test('should return an object with a function for each key in the theme, this function when provided a prop will return the prop.theme[functitonName][functionArgument]', () => {
   expect(color`main`(props)).toEqual('red')
   expect(color`secondary`(props)).toEqual('black')
   expect(padding`small`(props)).toEqual('3px')
+})
+
+test('should support nested keys', () => {
+  expect(blob`foo.bar`(props)).toEqual('yolo')
 })
