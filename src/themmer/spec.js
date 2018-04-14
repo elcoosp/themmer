c2cfffd9ef7accd6d1699c51ae16b9639e811d09
Elcoosp <elcoosp@gmail.com>
2018-04-14 23:11:25 +0200

    test: new test for reimplementation

--- src/themmer/spec.js
+++ src/themmer/spec.js
@@ -1,28 +1,25 @@
 const themmer = require('./index')
-
 const theme = {
   color: {
-    main: 'red',
-    secondary: 'black'
-  },
-  blob: {
-    foo: {
-      bar: 'yolo'
+    main: {
+      dark: 'black'
+    },
+    secondary: {
+      beautiful: {
+        someColor: 'red'
+      }
     }
-  },
-  padding: { small: '3px' }
+  }
 }
 
+const { color } = themmer(theme)
 const props = { theme }
 
-const { color, padding, blob } = themmer(theme)
-
-test('should return an object with a function for each key in the theme, this function when provided a prop will return the prop.theme[functitonName][functionArgument]', () => {
-  expect(color`main`(props)).toEqual('red')
-  expect(color`secondary`(props)).toEqual('black')
-  expect(padding`small`(props)).toEqual('3px')
+test('when called against a props theme should return the corresponding value in the theme', () => {
+  expect(color.secondary.beautiful.someColor(props)).toEqual('red')
+  expect(color.main.dark(props)).toEqual('black')
 })
-
-test('should support nested keys', () => {
-  expect(blob`foo.bar`(props)).toEqual('yolo')
+test('should throw if called with a key not in the theme', () => {
+  expect(() => color.secondary.beautiful.someColor.errror(props)).toThrowError()
+  expect(() => color.wrongProp.beautiful.someColor(props)).toThrowError()
 })
