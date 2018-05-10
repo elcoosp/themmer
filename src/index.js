650db9a99cf0b1daf426f9bb426ca0360f29e8df
Elcoosp <elcoosp@gmail.com>
2018-05-10 18:54:52 +0200

    refactor: inline function and extract the string directly from the strTemplate array

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
-module.exports = {
-  themmer: require('./themmer')
+'use strict'
+
+const themmer = strTemplate => props => {
+  const result = strTemplate[0] // Path in dot notation, eg : 'some.thing'
+    .split('.')
+    .reduce((acc, key) => acc[key], props.theme)
+
+  if (!result)
+    throw new Error(
+      `Could not access ${pathDotNotation} on props.theme, it is undefined`
+    )
+  else return result
 }
+
+module.exports = themmer
